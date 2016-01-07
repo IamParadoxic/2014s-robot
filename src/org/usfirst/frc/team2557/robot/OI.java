317ce57bd5c8198a46acb4f0a2c9bbb566d019b9
Harrison <Harrison@global-4646>
2016-01-07 12:57:41 -0800

    Added a comment

--- src/org/usfirst/frc/team2557/robot/OI.java
+++ src/org/usfirst/frc/team2557/robot/OI.java
@@ -18,7 +18,7 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-    
+    //Yo
 	//Gamepads
 	public Joystick gamepad1;
     public Joystick gamepad2;
